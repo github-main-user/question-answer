62296b3f1883d6074fecd7fd96fd4f54da97d5a4
github-main-user <kolya.skripn@mail.ru>
2025-11-13 14:05:36 +0300

    feat: register models in admin panel

--- qa/admin.py
+++ qa/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Answer, Question
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("text",)
+    search_fields = ("text",)
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("question__text", "user", "text")
+    search_fields = ("question__text", "text")
+    list_filter = ("user",)

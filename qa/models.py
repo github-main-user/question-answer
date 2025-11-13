c55f26932286779c6b09ce7d0bf840898e9c8f90
github-main-user <kolya.skripn@mail.ru>
2025-11-13 11:32:31 +0300

    feat: create question and answer models

--- qa/models.py
+++ qa/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class TimeStampModel(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True, help_text="Date and time when the object was created."
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, help_text="Date and time when the object was last updated."
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Question(TimeStampModel):
+    text = models.TextField(blank=False, help_text="Question content")
+
+
+class Answer(TimeStampModel):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name="answers",
+        help_text="Related question",
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="answers",
+        help_text="Author of the answer",
+    )
+    text = models.TextField(blank=False, help_text="Answer content")
